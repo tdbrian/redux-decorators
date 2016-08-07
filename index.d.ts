65e477708613226e981f744e8a5718a761d2865c
Thomas Brian <tbrian@appriver.com>
2016-08-06 21:03:52 -0500

    Add Store to index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -4,6 +4,7 @@ export interface RootReducer {
 export function Slice(slice: string, initialState?: any): Function;
 export function InitialState(state: any): Function;
 export function Reducer(...name: string[]): Function;
+export function State(...name: string[]): Function;
 export function Store(...name: string[]): Function;
 export interface IStore {
     dispatch(action: string, ...data: any[])
